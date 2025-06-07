7ccb60acdcd834e95c6a18af1c3924ccef3028bf
Houston M Vaughn <s377726@students.cdu.edu.au>
2025-06-07 07:45:09 +0000

    Redo

--- controllers/mouse.php
+++ controllers/mouse.php
@@ -37,7 +37,9 @@ function compile_app(&$app)
     //  Used to finalise the app function. Defines the function render and set_message
     //  This are called when callback is trigged in resolve. 
     $app->render = function ($layout, $content, $data = []) use (&$app) {
-        DEFINE("VIEWS", realpath(__DIR__ . '/../views'));
+        if (!defined('VIEWS')) {
+            DEFINE("VIEWS", realpath(__DIR__ . '/../views'));
+        }
         DEFINE("PARTIALS", VIEWS . '/partials');
 
         foreach (get_object_vars($app) as $key => $value) {
